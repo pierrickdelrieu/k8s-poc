8d1e371bf5fe1e7f81bc545e1a9abe0bcc2f6916
Pierrick Delrieu <58939886+pierrickdelrieu@users.noreply.github.com>
2023-05-22 10:31:08 +0200

    Delete addHaborCert.sh

--- wordpress/templates/addHaborCert.sh
+++
@@ -1,7 +0,0 @@
-sudo scp vagrant@192.168.50.100:/etc/ssl/certs/ca.crt /etc/ssl/certs/caHarbor.crt
-sudo cp /etc/ssl/certs/caHarbor.crt /usr/local/share/ca-certificates/
-sudo mkdir -p /var/lib/containerd/tls/
-sudo cp /etc/ssl/certs/caHarbor.crt /var/lib/containerd/tls/
-sudo chmod 644 /var/lib/containerd/tls/caHarbor.crt
-sudo systemctl restart containerd
-sudo update-ca-certificates
